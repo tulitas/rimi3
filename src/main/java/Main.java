1c1d576be73615820183ea449472417919fdba2c
tulitas <tulitas1@gmail.com>
2021-01-14 15:59:01 +0200

    task 3 work

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,47 @@
 import java.util.HashMap;
 import java.util.Map;
 
-class Main
-{
+class Main {
     public static void main(String[] args) {
 
-        int []a = {2,45,7,3,5,1,8,9, -34};
-        printSumPairs(a,11);
-
+        int[] digits = {2, 45, 7, 3, 5, 1, 8, 9, -34};
+        int result = 100;
+        printSumPairs(digits, result);
+        findPair(digits, result);
     }
 
 
-    public static void printSumPairs(int []input, int k){
+    public static void printSumPairs(int[] input, int result) {
         Map<Integer, Integer> pairs = new HashMap<Integer, Integer>();
 
-        for(int i=0;i<input.length;i++){
-
-            if(pairs.containsKey(input[i]))
-                System.out.println(input[i] +", "+ pairs.get(input[i]));
-            else
-                pairs.put(k-input[i], input[i]);
+        for (int j : input) {
+            if (pairs.containsKey(j)) {
+                System.out.println(j + " + " + pairs.get(j) + " = " + result);
+            } else
+                pairs.put(result - j, j);
         }
 
     }
-}
+
+    public static void findPair(int[] input, int result) {
+        int pairsCount = 0;
+        for (int i = 0; i < input.length - 1; i++) {
+            for (int j = i + 1; j < input.length; j++) {
+                if (input[i] + input[j] == result) {
+                    System.out.println("Pair found at index " + i + " and " + j);
+                    pairsCount++;
+                }
+
+                }
+
+            }
+        if (pairsCount == 0)
+            System.out.println("Pair not found");
+        }
+
+
+    }
+
+
+
+
